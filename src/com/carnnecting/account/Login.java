92f7c5489ff2f5876715fcff358335cdd0844a07
Vijay Raghavan <vijay.raghavan@sv.cmu.edu>
2013-04-21 11:44:47 -0700

    Added Facebook login/logout mechanism

--- src/com/carnnecting/account/Login.java
+++ src/com/carnnecting/account/Login.java
@@ -2,40 +2,41 @@ package com.carnnecting.account;
 
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.content.Intent;
 import android.view.Menu;
-import android.widget.Button;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import com.carnnecting.home.Home;
+import android.support.v4.app.FragmentActivity;
+import com.carnnecting.ws.FBConnect;
 import com.cmu.carnnecting.R;
 
+public class Login extends FragmentActivity {
 
-public class Login extends Activity {
+	private FBConnect mainFragment;
 
-	private Button demoButton;
-	
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		System.out.println("Login: Debug Message");
+		
+		if (savedInstanceState == null) {
+	        // Add the fragment on initial activity setup
+	        mainFragment = new FBConnect();
+	        getSupportFragmentManager()
+	        .beginTransaction()
+	        .add(android.R.id.content, mainFragment)
+	        .commit();
+	    } else {
+	        // Or set the fragment from restored state info
+	        mainFragment = (FBConnect) getSupportFragmentManager()
+	        .findFragmentById(android.R.id.content);
+	    }
 		setContentView(R.layout.activity_carnnecting_main);
 		
-		demoButton = (Button) findViewById(R.id.demoOnlyButton);
-		
-		demoButton.setOnClickListener(new View.OnClickListener() {
-			public void onClick(View v) {
-				Intent intent = new Intent(v.getContext(), Home.class);
-				startActivity(intent);
-			}
-		});
-		
-		//test comment
-		
 	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.carnnecting_main, menu);
+		return true;
+	}
+	
 }
