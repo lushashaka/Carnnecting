9cb68271f7c07a5786d3ec355cb565b89af0f3c5
Vijay Raghavan <vijay.raghavan@sv.cmu.edu>
2013-04-23 18:40:40 -0700

    Added comment for hash code and removed unused imports

--- src/com/carnnecting/account/Login.java
+++ src/com/carnnecting/account/Login.java
@@ -1,16 +1,9 @@
 package com.carnnecting.account;
 
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.util.Base64;
+
 import android.util.Log;
 import android.view.Menu;
 
@@ -26,20 +19,6 @@ public class Login extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.i(TAG, "Login Screen");
-		/*try {
-		       PackageInfo info = getPackageManager().getPackageInfo(
-		               "com.cmu.carnnecting", 
-		               PackageManager.GET_SIGNATURES);
-		       for (Signature signature : info.signatures) {
-		           MessageDigest md = MessageDigest.getInstance("SHA");
-		           md.update(signature.toByteArray());
-		           Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-		           }
-		   } catch (NameNotFoundException e) {
-
-		   } catch (NoSuchAlgorithmException e) {
-
-		   }*/
 		
 		/* Use this code to generate hash
 		  try {
