a2d1099ea219b5a73cd663231aa51237e2e7c351
lushashaka <lushazhang@gmail.com>
2013-04-23 18:19:16 -0700

    Fixed Hashcode issue for FB login

--- src/com/carnnecting/account/Login.java
+++ src/com/carnnecting/account/Login.java
@@ -1,10 +1,19 @@
 package com.carnnecting.account;
 
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
-import android.support.v4.app.FragmentActivity;
+
 import com.carnnecting.ws.FBConnect;
 import com.cmu.carnnecting.R;
 
@@ -17,6 +26,20 @@ public class Login extends FragmentActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.i(TAG, "Login Screen");
+		/*try {
+		       PackageInfo info = getPackageManager().getPackageInfo(
+		               "com.cmu.carnnecting", 
+		               PackageManager.GET_SIGNATURES);
+		       for (Signature signature : info.signatures) {
+		           MessageDigest md = MessageDigest.getInstance("SHA");
+		           md.update(signature.toByteArray());
+		           Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+		           }
+		   } catch (NameNotFoundException e) {
+
+		   } catch (NoSuchAlgorithmException e) {
+
+		   }*/
 		
 		if (savedInstanceState == null) {
 	        // Add the fragment on initial activity setup
