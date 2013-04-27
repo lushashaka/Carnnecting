c7cfb46d06750ec61d511fc13e0a5a8b734d6f5e
lushashaka <lushazhang@gmail.com>
2013-04-26 19:04:54 -0700

    minor fix

--- src/com/carnnecting/category/CategoryDetail.java
+++ src/com/carnnecting/category/CategoryDetail.java
@@ -53,7 +53,6 @@ public class CategoryDetail extends ListActivity {
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		//setContentView(R.layout.activity_category_detail);
 		ActionBar actionBar = getActionBar();
 		actionBar.setDisplayHomeAsUpEnabled(true);
 		
